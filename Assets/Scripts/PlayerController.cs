b80412c902743f10ac1d561f9d9c8218ed724152
Sdesser <dextrous90@gmail.com>
2019-06-27 12:57:41 +0300

    WeHaveMovement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Vector2 movementDirection;
+    //public GameObject player;
+    public Rigidbody2D playerRB;
+
+    public float movementSpeedMult = 1.0f;
+    public float movementSpeed;
+    public float sprintMult = 1.0f;
+    
+    void Awake()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        ProcessInputs();
+        Move();
+    }
+
+    void ProcessInputs()
+    {
+        movementDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        movementSpeed = Mathf.Clamp(movementDirection.magnitude, 0.0f, 1.0f);
+        movementDirection.Normalize();
+    }
+
+    void Move()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            sprintMult = 2.0f;
+        }
+
+        else
+        {
+            sprintMult = 1.0f;
+        }
+
+        // new Vector3(Mathf.Lerp(minimum, maximum, t), 0, 0)
+
+        playerRB.velocity = movementDirection * movementSpeed * movementSpeedMult * sprintMult;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    public Rigidbody2D playerRigidbody;
+
+    [SerializeField] private float movementSpeed = 1.0f;
+    [SerializeField] private float maxMovementSpeed = 5.0f;
+    [SerializeField] private float reducedMovementSpeed = 0.5f;
+
+    void Awake()
+    {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    public void MovePlayer()
+    {
+        //playerRigidbody.velocity = new Vector2(movementSpeed, playerRigidbody.velocity.y);
+
+
+    }
+}
