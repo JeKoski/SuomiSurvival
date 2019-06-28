b8543a86240d0d9d88ff44a28d7996d063c2d3ee
Sdesser <dextrous90@gmail.com>
2019-06-28 12:57:51 +0300

    Stuffs

--- Assets/Scripts/GameplayMechanics.cs
+++ Assets/Scripts/GameplayMechanics.cs
@@ -4,20 +4,27 @@ using UnityEngine;
 
 public class GameplayMechanics : MonoBehaviour
 {
-    [Header("Necessary")]
+    [Header("References:")]
     [SerializeField] private GameplayVariables gpv;
 
-    
-    
-    
     void Start()
     {
-        
+        gpv.ResetAll();
     }
-
     
     void Update()
     {
-        
+        PlayerStatsUpdate();
+    }
+
+    void PlayerStatsUpdate()
+    {
+        gpv.playerCoffee = gpv.playerCoffee + Time.deltaTime * gpv.playerCoffeeRate;
+        gpv.playerWarmth = gpv.playerWarmth + Time.deltaTime * gpv.playerWarmthRate;
+        gpv.playerHunger = gpv.playerHunger + Time.deltaTime * gpv.playerHungerRate;
+        gpv.playerThirst = gpv.playerThirst + Time.deltaTime * gpv.playerThirstRate;
+
+
+        //if (player is in mosquito field) {gpv.playerMosquitoes = gpv.playerMosquitoes - Time.deltaTime * gpv.playerMosquitoesRate;}
     }
 }
---
+++ Assets/Scripts/MeterController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MeterController : MonoBehaviour
+{
+    [Header("References:")]
+    [SerializeField] private GameplayVariables gpv;
+    [SerializeField] private Slider healthMeter;
+    [SerializeField] private Slider coffeeMeter;
+    [SerializeField] private Slider warmthMeter;
+    [SerializeField] private Slider mosquitoesMeter;
+    [SerializeField] private Slider hungerMeter;
+    [SerializeField] private Slider thirstMeter;
+    
+    void Update()
+    {
+        UpdateMeters();
+    }
+
+    void UpdateMeters()
+    {
+        healthMeter.value = gpv.playerHealth;
+        coffeeMeter.value = gpv.playerCoffee;
+        warmthMeter.value = gpv.playerWarmth;
+        mosquitoesMeter.value = gpv.playerMosquitoes;
+        hungerMeter.value = gpv.playerHunger;
+        thirstMeter.value = gpv.playerThirst;
+    }
+}
