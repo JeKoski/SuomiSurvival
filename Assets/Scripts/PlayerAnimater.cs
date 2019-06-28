866c0299045fea9285b2c7897798e90c20b3852f
Maistelujaska <50902126+Maistelujaska@users.noreply.github.com>
2019-06-28 12:54:04 +0300

    AnimationWIP

---
+++ Assets/Scripts/PlayerAnimater.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimater : MonoBehaviour
+{
+    public Animator animator;
+    [SerializeField] private PlayerController playerController;
+
+
+    private void Update()
+    {
+        Animate();
+    }
+
+    void Animate()
+    {
+        animator.SetFloat("Horizontal", playerController.movementDirection.x);
+        animator.SetFloat("Vertical", playerController.movementDirection.y);
+    }
+}
