45c703032e428828a700b4f7db3f6b137ffa4e78
Sdesser <dextrous90@gmail.com>
2019-07-10 12:57:20 +0300

    WeHaveDrunkDrivers

---
+++ Assets/Scripts/DrunkDriverSpawnerScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrunkDriverSpawnerScript : MonoBehaviour
+{
+    public GameObject drunkDriver;
+
+    [SerializeField] float spawnDelay = 2.0f;
+    [SerializeField] float minSpawnDelay = 10.0f;
+    [SerializeField] float maxSpawnDelay = 30.0f;
+    private float spawnTimer = 0.0f;
+
+    private void Start()
+    {
+        RNG();
+    }
+
+    void FixedUpdate()
+    {
+        spawnTimer += Time.deltaTime;
+
+        if (spawnTimer > spawnDelay)
+        {
+            RNG();
+            Instantiate(drunkDriver, transform.position, Quaternion.Euler(0, 0, 0));
+            spawnTimer = 0;
+        }
+    }
+
+    private void RNG()
+    {
+        spawnDelay = Random.Range(minSpawnDelay, maxSpawnDelay);
+    }
+}
