f889ff0945a3d3edb04d381b9470a42880952c81
Maistelujaska <50902126+Maistelujaska@users.noreply.github.com>
2019-07-11 11:32:36 +0300

    AnimationsAndMosquitoes

---
+++ Assets/Scripts/EnemyFollow.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour
+{
+    [SerializeField] private float enemySpeed;
+    [SerializeField] float stoppingDistance;
+
+    private Transform target;
+
+    private bool followingPlayer = false;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+
+    void Update()
+    {
+        if (followingPlayer)
+        {
+            if (Vector2.Distance(transform.position, target.position) > stoppingDistance)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, target.position, enemySpeed * Time.deltaTime);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Mosquito Field has detected you!");
+            followingPlayer = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("You have escaped the clutches of the blood thirsty buggers!");
+            followingPlayer = false;
+        }
+    }
+}
