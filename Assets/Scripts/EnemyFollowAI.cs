45819593835fafd921a90a93ffd4e22dd72c45fc
Maistelujaska <50902126+Maistelujaska@users.noreply.github.com>
2019-07-12 12:45:11 +0300

    EnemyAIwip

---
+++ Assets/Scripts/EnemyFollowAI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollowAI : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private Transform target;
+    private Vector2 targetLocation;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private Rigidbody2D rb;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void MoveTo()
+    {
+
+    }
+
+    void UpdatePlayerLocation()
+    {
+        targetLocation = player.GetComponent<Transform>().position;
+    }
+
+}
