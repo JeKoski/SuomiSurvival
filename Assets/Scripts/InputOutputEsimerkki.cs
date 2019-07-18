1975ddc26ffc8471497f9852ac1e04674b671cf9
Sdesser <dextrous90@gmail.com>
2019-07-18 10:05:39 +0300

    esimerkki

---
+++ Assets/Scripts/InputOutputEsimerkki.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputOutputEsimerkki : MonoBehaviour
+{
+    [SerializeField] private int minimi;
+    [SerializeField] private int maksimi;
+    private int enemySpeed;
+
+    private void Update()
+    {
+        enemySpeed = RandomNumero(minimi, maksimi);
+    }
+
+
+    int RandomNumero(int randomMin, int randomMax)
+    {
+        int randomNumero;
+        
+        randomNumero = Random.Range(randomMin, randomMax);
+
+        return randomNumero;
+    }
+}
