6fba4a5c6478449ec8f2caf97a36567c3f7f5f52
Maistelujaska <50902126+Maistelujaska@users.noreply.github.com>
2019-08-01 10:39:36 +0300

    Swimming

--- Assets/Scripts/PlayerAnimater.cs
+++ Assets/Scripts/PlayerAnimater.cs
@@ -6,7 +6,7 @@ public class PlayerAnimater : MonoBehaviour
 {
     public Animator animator;
     [SerializeField] private PlayerController playerController;
-
+    [SerializeField] private GameplayMechanics gpm;
 
     private void Update()
     {
@@ -15,13 +15,25 @@ public class PlayerAnimater : MonoBehaviour
 
     void Animate()
     {
-        if (playerController.movementDirection != Vector2.zero)
+
+       if (playerController.movementDirection != Vector2.zero)
         {
         animator.SetFloat("Horizontal", playerController.movementDirection.x);
         animator.SetFloat("Vertical", playerController.movementDirection.y);
 
         }
-        
         animator.SetFloat("Speed", playerController.movementSpeed);
+
+        if (gpm.playerInWater)
+        {
+            animator.SetLayerWeight(animator.GetLayerIndex("Walking"), 0f);
+            animator.SetLayerWeight(animator.GetLayerIndex("Swimming"), 1f);
+        }
+
+        else if (!gpm.playerInWater)
+        {
+            animator.SetLayerWeight(animator.GetLayerIndex("Walking"), 1f);
+            animator.SetLayerWeight(animator.GetLayerIndex("Swimming"), 0f);
+        }
     }
 }
